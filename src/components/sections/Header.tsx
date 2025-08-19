55ceab2463be590996086e62dd444345079ff2d8
rochamaatheus <matheussilveirarocha.sc@gmail.com>
2025-08-19 14:40:09 -0300

    Ajuste

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -21,7 +21,7 @@ const Header = () => {
           <div className="flex items-center space-x-2">
             <div className="w-10 h-10 bg-hero-gradient rounded-lg flex items-center justify-center">
               <span className="text-primary-foreground font-bold text-xl">
-                JS
+                MC
               </span>
             </div>
             <div>
